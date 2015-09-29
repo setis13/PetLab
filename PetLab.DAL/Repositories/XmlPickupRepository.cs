f428c9dec5ed599898fc938f3b5e2a347af13d41
pilipenko.anton <pilipenko.anton@gmail.com>
2015-09-29 22:08:12 +0600

    - иправил ошибку, когда файл съёма имел существующее имя

--- PetLab.DAL/Repositories/XmlPickupRepository.cs
+++ PetLab.DAL/Repositories/XmlPickupRepository.cs
@@ -5,14 +5,33 @@ using PetLab.DAL.Contracts.Context;
 using PetLab.DAL.Models;
 using PetLab.DAL.Models.xml;
 using PetLab.DAL.Repositories.Base;
+using System.Threading;
 
 namespace PetLab.DAL.Repositories {
 	public class XmlPickupRepository : XmlRepositoryWriter<pickupXml> {
 		public XmlPickupRepository(IPetLabXmlContext context) : base(context) {
 		}
 
+		private object _lock = new object();
+		private DateTime oldQueryDateTime;
+
+		/// <summary>
+		/// возвращает подстроку для файла-запроса.
+		/// к подстроке добавляется секунда, если она уже генерировалась
+		/// </summary>
 		protected override string GenerateQuerySubstring() {
-			return DateTime.Now.ToString("yyyyMMddHHmmss");
+			lock (_lock) {
+				var queryDateTime = DateTime.Now;
+				if (queryDateTime.Second == oldQueryDateTime.Second &&
+					queryDateTime.Minute == oldQueryDateTime.Minute &&
+					queryDateTime.Hour == oldQueryDateTime.Hour) {
+					var timeSpan = TimeSpan.FromSeconds(1);
+					queryDateTime = queryDateTime + timeSpan;
+					Thread.Sleep(timeSpan);
+				}
+				oldQueryDateTime = queryDateTime;
+				return queryDateTime.ToString("yyyyMMddHHmmss");
+			}
 		}
 
 		protected override bool FileCheckContent(string content, pickupXml entry) {
